2f26e36058b1613be381f754a7c5ec1fcfaaf8a7
Dola Datta <dola.datta@gmail.com>
2020-09-11 12:59:27 +0530

    Fibonacci

--- src/com/intrvw/prgrm/code/FibonacciSeries.java
+++ src/com/intrvw/prgrm/code/FibonacciSeries.java
@@ -15,7 +15,39 @@ Read more: https://javarevisited.blogspot.com/2017/07/top-50-java-programs-from-
 public class FibonacciSeries {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// How many fibonacci numbers
+		int noOfFibonacciNos = 10;
+		int a1 = 1;
+		int a2 = 1;
+		int index = 1;
+		while(index <= noOfFibonacciNos) {
+			if(index == 1) System.out.println(index + " : " + a1);
+			else if(index == 2) System.out.println(index + " : " + a2);
+			else {
+				int next = a1 + a2;
+				System.out.println(index + " : " + next);
+				a1 = a2;
+				a2 = next;
+			}
+			++index;
+		}
+		System.out.println("--------------------------");
+		// Fibonacci less than a number
+		int fibonacciLessThanNo = 150;
+		a1 = 1;
+		a2 = 1;
+		index = 1;
+		while (a1+a2 < fibonacciLessThanNo) {
+			if(index == 1) System.out.println(index + " : " + a1);
+			else if(index == 2) System.out.println(index + " : " + a2);
+			else {
+				int next = a1 + a2;
+				System.out.println(index + " : " + next);
+				a1 = a2;
+				a2 = next;
+			}
+			++index;
+		}
 
 	}
 
